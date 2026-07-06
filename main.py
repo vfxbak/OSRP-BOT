215b96abeddc68207dd73a05cf85b277dadaa47a
vfxbak <vfxbak@users.noreply.github.com>
2026-07-06 15:51:58 +0100

    fix: add message_content intent for reading Circle bot messages

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ TOKEN = os.environ.get("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.message_content = True
 intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
